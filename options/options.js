52bb0b1455780c77f21fad624323e1a0ddcad299
Crowdbotics <team@crowdbotics.com>
2023-08-09 20:37:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "poacherchat-42754",
+  url: "https://poacherchat-42754.botics.co",
+  api: "https://poacherchat-42754.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
